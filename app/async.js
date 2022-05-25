86621b86198a39f92b94e84f5bb332b6e1a0bd47
Tushar Joy <tj@intop.no>
2022-05-25 16:22:41 +0600

    solved async and best practices problems

--- app/async.js
+++ app/async.js
@@ -2,10 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise((resolve, reject) => {
+      resolve(value);
+    });
   },
 
-  manipulateRemoteData: function(url) {
+  manipulateRemoteData: function (url) {
 
+    return new Promise((resolve, reject) => {
+      const response = fetch(url);
+      response.then(res => res.json())
+        .then(parsed => { 
+          let data = parsed.people.map(item=>item.name);
+          data = data.sort();
+          resolve(data); 
+        })
+    })
   }
-};
+}; 
+ 
